625e488ea26da8aab10f0404ecbb0c035d4931aa
Demis Bellot <demis.bellot@gmail.com>
2019-05-03 18:54:48 -0400

    go back to using manual app.nuspec

--- src/WebApp/install-local-tool.bat
+++ src/WebApp/install-local-tool.bat
@@ -1,4 +1,4 @@
-REM dotnet pack WebApp.csproj -c release -o nupkg
 CALL pack.bat
+REM dotnet pack WebApp.csproj -c release -o nupkg
 dotnet tool uninstall -g app
 dotnet tool install --add-source .\nupkg -g app
