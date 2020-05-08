1575b92ad105e4ede0c7bdb6814cdfc153b41f9c
Demis Bellot <demis.bellot@gmail.com>
2020-05-09 01:07:48 +0800

    add copy sharpdata .bat

---
+++ src/Test/apps/copy.bat
@@ -0,0 +1,8 @@
+copy multitenancy\_table.html ..\..\..\..\NetCoreTemplates\rockwind-app\sharpdata\
+copy multitenancy\_table.html ..\..\..\..\sharp-apps\rockwind\sharpdata\
+copy multitenancy\_table.html ..\..\..\..\sharp-apps\rockwind\sharpdata\
+copy multitenancy\_table.html ..\..\..\..\sharp-apps\rockwind-aws\app\sharpdata\
+copy multitenancy\_table.html ..\..\..\..\sharp-apps\rockwind-azure\app\sharpdata\
+copy multitenancy\_table.html ..\..\..\..\mix\apps\rockwind\sharpdata\
+copy multitenancy\_table.html ..\..\..\..\mix\apps\sharpdata\
+copy multitenancy\_db\_table.html ..\..\..\..\mix\apps\sharpdata\_db\
