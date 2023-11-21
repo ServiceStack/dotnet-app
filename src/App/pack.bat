06d0839568d965d012d4fdf9805e5c9a4840513b
Demis Bellot <demis.bellot@gmail.com>
2023-11-21 19:40:30 +0800

    Upgrade to .NET 8

--- src/App/pack.bat
+++ src/App/pack.bat
@@ -1,5 +1,5 @@
 REM dotnet pack WebApp.csproj -c release -o nupkg
 dotnet pack -c Release
 pushd nupkg
-nuget pack ../app.nuspec -Properties "cefdir=C:\src\ServiceStack.CefGlue\src\ServiceStack.CefGlue.Win64"
+nuget pack ../app.nuspec -Properties "cefdir=C:\src\ServiceStack\ServiceStack.CefGlue\src\ServiceStack.CefGlue.Win64"
 popd
